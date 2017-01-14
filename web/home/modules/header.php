6d37985fb73249d5845ac1b5b925fea3076012a6
WhiteZane <whi12059@byui.edu>
2017-01-14 14:15:23 -0700

    fixed links

--- web/home/modules/header.php
+++ web/home/modules/header.php
@@ -7,12 +7,12 @@ $name = "Zane White" ;
                     <p class="navbar-brand">CS313</p>
                   </div>
                   <ul class="nav navbar-nav">
-                    <li class=""><a href="#">Home</a></li>
+                    <li class=""><a href="http://lit-refuge-30680.herokuapp.com/home">Home</a></li>
                     <li class="dropdown">
                       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Assignments
                       <span class="caret"></span></a>
                       <ul class="dropdown-menu">
-                        <li><a href="assignments/index.php">Home</a></li>
+                        <li><a href="http://lit-refuge-30680.herokuapp.com/home/assignments/index.php">Home</a></li>
                         <li><a href="#">Assignment 2</a></li>
                         <li><a href="#">Assignment 3</a></li>
                       </ul>
